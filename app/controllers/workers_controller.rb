b8f9d03e37f39a93f1b1705b23d9a615021c8b6f
cirssax <cristobalisr@gmail.com>
2018-12-03 00:54:58 -0300

    CRUD de usuarios terminado, falta agregar estilos

--- app/controllers/workers_controller.rb
+++ app/controllers/workers_controller.rb
@@ -1,24 +1,44 @@
 class WorkersController < ApplicationController
 
-  def ListarUsuarios
+  def index
     @Usuarios = Worker.all
   end
 
-  def CrearUsuario
+  def new
     @ListaRoles = Role.all
-    @ListaEstado = Array.new
-    @ListaEstado = [[:name =>"ACTIVO", :id=>"1"][:name =>"INACTIVO", :id=>"0"]]
+    @ListaEstado = State.all
+    @worker = Worker.new
   end
 
   def create
     #render plain: params[:worker].inspect
-    @worker = Worker.new worker_params
+    @worker = Worker.new(worker_params)
+    if @worker.save
+        redirect_to workers_path
+    else
+       render CrearUsuario
+    end
 
+  end
+
+  def edit
+    @ListaRoles = Role.all
+    @ListaEstado = State.all
+    @worker = Worker.find(params[:id])
+  end
+
+  def update
+    @worker = Worker.find(params[:id])
+    if @worker.update(worker_params)
+      redirect_to workers_path
+    else
+      render :edit
+    end
   end
 
   private
   def worker_params
-    params.require(:worker).permit(:nombre_trabajador, :apellidos_trabajador, :rut, :dv,:fono, :domicilio, :id_rol, :estado)
+    params.require(:worker).permit(:nombre_trabajador, :apellidos_trabajador, :rut, :dv,:fono, :domicilio, :id_rol, :id_estado)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resource :workers
+  #resource :workers
   # nombre del metodo http + nombre del servicio o recurso (URL) + to: + nombre del controlador + # + metodo del controlador
-  get 'workers/ListaUsuarios', to: 'workers#ListarUsuarios'
-  get 'workers/Nuevo', to: 'workers#CrearUsuario'
-
+  get '/workers', to: 'workers#index'
+  get '/workers/new', to: 'workers#new'
+  post '/workers', to: 'workers#create' #Con este se crea el trabajador
+  get '/worker/:id/', to: 'workers#show', as: 'worker' #Para editar se necesita esta ruta, ya que se necesita la invocacion
+  get '/workers/:id/edit', to: 'workers#edit', as: 'edit_worker'
+  patch '/worker/:id', to: 'workers#update' #update
+  put '/worker/:id', to: 'workers#update' #update
 end
